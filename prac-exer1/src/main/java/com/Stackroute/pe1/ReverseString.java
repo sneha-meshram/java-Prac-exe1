0fc6c64bc594f517c82a48a39a512fd68c7aebc3
sneha <sneha.meshram@cgi.com>
2019-07-12 14:14:22 +0530

    test case added for reverse string

--- prac-exer1/src/main/java/com/Stackroute/pe1/ReverseString.java
+++ prac-exer1/src/main/java/com/Stackroute/pe1/ReverseString.java
@@ -4,22 +4,23 @@ import java.util.Scanner;
 
 public class ReverseString {
 
-    public static void main(String[] args) {
+    public String CheckStringIsReverse(String str) {
 
-        String str = "";
+      /*  String str = ""; */
         String revstr = "";
         int len;
 
-        System.out.println("enter a word");
-        Scanner input = new Scanner(System.in);
-        str = input.nextLine();
-
+        //
+        // System.out.println("enter a word");
+        // Scanner input = new Scanner(System.in);
+        // str = input.nextLine();
+        //
         len = str.length();
 
         for(int i=len - 1; i >= 0;i--)
         {
             revstr = revstr + str.charAt(i);
         }
-        System.out.println("the reverse string is " +revstr);
+        return(revstr);
     }
 }
--- prac-exer1/src/test/java/com/Stackroute/pe1/ReverseStringTest.java
+++ prac-exer1/src/test/java/com/Stackroute/pe1/ReverseStringTest.java
@@ -1,12 +1,31 @@
 package com.Stackroute.pe1;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class ReverseStringTest {
 
+ReverseString reverseString;
 
+@Before
+    public void setup()
+   {
+    reverseString= new ReverseString();
+   }
+@After
+    public void tearDown()
+   {
+    reverseString= null;
+   }
 
-
+   @Test
+           public void reverseString() {
+       //act
+       String Result = reverseString.CheckStringIsReverse("london");
+       //assert
+       assertEquals("nodnol", Result);
+   }
 }
