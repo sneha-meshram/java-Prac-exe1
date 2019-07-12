8df3ba613b8baa474d7455db269fd03374c615b0
sneha <sneha.meshram@cgi.com>
2019-07-12 14:58:38 +0530

    testcase added for VowelAndConsonent

--- prac-exer1/src/main/java/com/Stackroute/pe1/VowelConso.java
+++ prac-exer1/src/main/java/com/Stackroute/pe1/VowelConso.java
@@ -5,23 +5,20 @@ import java.util.Scanner;
 
 public class VowelConso {
 
-    public static void main(String[] args) {
-        Scanner s = new Scanner(System.in);
-        System.out.print("Enter a word: ");
-        String input = s.next();
+    public String Checkcharacter(String input) {
+
         if ((input != null) && (!input.isEmpty()) && (!input.matches(".\\d."))) {
             char[] inputArray = input.toCharArray();
             for (char c : inputArray) {
                 if (Character.toLowerCase(c) == 'a' || Character.toLowerCase(c) == 'e' || Character.toLowerCase(c) == 'i' || Character.toLowerCase(c) == 'o' || Character.toLowerCase(c) == 'u') {
-                    System.out.println(c + " - vowel");
+                    System.out.println(c + " is - vowel");
                 } else {
-                    System.out.println(c + " Consonant");
+                    System.out.println(c + " is - Consonant");
                 }
             }
         } else {
-            System.out.println("Please give a valid string");
+            return("Please give a valid string");
         }
-        /*Close the scanner*/
-        s.close();
+        return(input);
     }
 }
---
+++ prac-exer1/src/test/java/com/Stackroute/pe1/VowelConsoTest.java
@@ -0,0 +1,28 @@
+package com.Stackroute.pe1;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class VowelConsoTest {
+
+    VowelConso vowelconso;
+
+    @Before
+    public void setup()
+    {
+    vowelconso=new VowelConso();
+    }
+
+    @After
+    public void tearDown()
+    {
+    vowelconso=null;
+    }
+
+    @Test
+    public void Checkcharacter(){
+     }
+}
